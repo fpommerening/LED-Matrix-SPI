72c4ee649e3c4b222ee5973d60f457d28338c92b
Frank Pommerening <frank@pommerening-online.de>
2018-06-05 20:11:37 +0200

    color sample

--- RaspberryPi/Color.c
+++ RaspberryPi/Color.c
@@ -15,6 +15,40 @@ int main(void)
     wiringPiSetup();
     wiringPiSPISetup(0, 500000);
 
+    void color()
+    {
+        int shift;
+        int colorselect;
+        int rate;
+        int colorshift;
+        int colorrepeat;
+
+        uint16_t color = 0x0000;
+
+        for (colorshift = 0; colorshift < 13; colorshift++)
+        {
+            color = color | (0x01 << colorshift);
+
+            for (colorrepeat = 0; colorrepeat < 30; colorrepeat++)
+            {
+                for (rate = 0; rate < 4; rate++)
+                {
+                    for (colorselect = 0; colorselect < 3; colorselect++)
+                    {
+                        for (shift = 0; shift < 8; shift++)
+                        {
+                            data[RED_DATA] = (colorselect == 0 && ((color >> rate) & 0x0001)) ? 0x00 : 0xFF;
+                            data[BLUE_DATA] = (colorselect == 1 && ((color >> 4 + rate) & 0x0001)) ? 0x00 : 0xFF;
+                            data[GREEN_DATA] = (colorselect == 2 && ((color >> 8 + rate) & 0x0001)) ? 0x00 : 0xFF;
+                            data[3] = 0x01 << shift;
+                            wiringPiSPIDataRW(0, data, sizeof(data));
+                        }
+                    }
+                }
+            }
+        }
+    };
+
     void matrixoff()
     {
         int j;
@@ -34,6 +68,7 @@ int main(void)
     {
 
         matrixoff();
-        delay(100);
+        delay(1000);
+        color();
     }
 }
