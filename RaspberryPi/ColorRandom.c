4abd629a1fc84c0d2357b236cf8c20dd0ba70b31
Frank Pommerening <frank@pommerening-online.de>
2018-06-05 20:41:41 +0200

    Add color random

---
+++ RaspberryPi/ColorRandom.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+#include <stdint.h>
+
+#include <stdlib.h>
+#include <time.h>
+
+#define RED_DATA 0
+#define BLUE_DATA 1
+#define GREEN_DATA 2
+
+int main(void)
+{
+  static uint8_t data[4] = {0x0, 0x0, 0x0, 0x0};
+
+  wiringPiSetup();
+  wiringPiSPISetup(0, 500000);
+
+  void color()
+  {
+    int shift;
+    int colorselect;
+    int rate;
+    int colorrepeat;
+
+    srand(time(NULL));
+
+    uint16_t color = rand() % 4096;
+
+    for (colorrepeat = 0; colorrepeat < 30; colorrepeat++)
+    {
+      for (rate = 0; rate < 4; rate++)
+      {
+        for (colorselect = 0; colorselect < 3; colorselect++)
+        {
+          for (shift = 0; shift < 8; shift++)
+          {
+            data[RED_DATA] = (colorselect == 0 && ((color >> rate) & 0x0001)) ? 0x00 : 0xFF;
+            data[BLUE_DATA] = (colorselect == 1 && ((color >> 4 + rate) & 0x0001)) ? 0x00 : 0xFF;
+            data[GREEN_DATA] = (colorselect == 2 && ((color >> 8 + rate) & 0x0001)) ? 0x00 : 0xFF;
+            data[3] = 0x01 << shift;
+            wiringPiSPIDataRW(0, data, sizeof(data));
+          }
+        }
+      }
+    }
+  };
+
+  void matrixoff()
+  {
+    int j;
+    int x = 2;
+    for (j = 0; j < 8; j++)
+    {
+      data[RED_DATA] = 0xFF;
+      data[BLUE_DATA] = 0xFF;
+      data[GREEN_DATA] = 0xFF;
+      data[3] = 0x01 << j;
+      wiringPiSPIDataRW(0, data, sizeof(data));
+      delay(x);
+    }
+  }
+
+  while (1)
+  {
+    color();
+  }
+}
